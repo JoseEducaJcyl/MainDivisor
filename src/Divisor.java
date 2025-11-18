5b043863b934ca87a4cc866e7785bfdc580c2a05
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2025-11-18 13:00:36 +0100

    Decimoctavo ejercicio del bloque 2 (POO)

--- src/Divisor.java
+++ src/Divisor.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
+// Clase Divisor con el metodo dividir
 public class Divisor {
+    // Si se intenta dividir entre 0 lanza una excepcion
+    public int dividir(int num1, int num2) throws ArithmeticException {
+        if(num1==0 || num2==0){
+            throw new ArithmeticException();
+        }else if(num1==num2){
+            return 1;
+        }else {
+            return num1/num2;
+        }
+    }
 }
